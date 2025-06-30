0304e3688fa91041d78ba577a2d49bc1fe98e0f1
Anna <anna0983266723@gmail.com>
2025-06-30 19:15:12 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace 第一站1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名：張揚恩          術科測試編號：\r\n\r\n座號：                      日期：2025/07/06";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "第一題結果：\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "第一題結果：\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "第一題結果：\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 299);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "第一題結果：\r\n";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 344);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "第一題結果：\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 562);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace 第一站1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Palindrome(string input)
+        {
+            int inp_size = input.Length;
+            int mid = inp_size % 2 == 0 ? (inp_size - 1) / 2 : (inp_size - 1) / 2 + 1;
+            for (int i = 0; i < mid; i++)
+            {
+                if (input[inp_size - 1 - i] != input[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool Prime(string input)
+        {
+            int in_inpu = int.Parse(input);
+            int input_size = input.Length - 1;
+            for (int i = 2; i < input_size; i++)
+            {
+                if (in_inpu % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //*******************************
+            //* 11900-1060301 Program Start *
+            //*******************************
+            string input1 = File.ReadAllText("1060301.SM");
+            if (Palindrome(input1))
+            {
+                label2.Text = $"第一題結果：{input1} is a palindrome.";
+            }
+            else
+            {
+                label2.Text = $"第一題結果：{input1} is not a palindrome.";
+            }
+            //*******************************
+            //* 11900-1060302 Program Start *
+            //*******************************
+            int max = int.Parse(File.ReadAllText("1060302.SM"));
+            label3.Text = "第二題結果：\n";
+            for (int i = 1; i <= max; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    label3.Text += j;
+                }
+                label3.Text += "\n";
+            }
+            //*******************************
+            //* 11900-1060303 Program Start *
+            //*******************************
+            string input3 = File.ReadAllText("1060303.SM");
+            if (Prime(input3))
+            {
+                label4.Text = $"第三題結果：{input3} is a prime number.";
+            }
+            else
+            {
+                label4.Text = $"第三題結果：{input3} is not a prime number.";
+            }
+            //*******************************
+            //* 11900-1060304 Program Start *
+            //*******************************
+            string[] input4 = File.ReadAllLines("1060304.SM");
+            double[] answ = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                string[] strings = input4[i].Split(',');
+                answ[i] = int.Parse(strings[1]) / ((int.Parse(strings[0]) / 100.0) * (int.Parse(strings[0]) / 100.0));
+            }
+            Array.Sort(answ);
+            if (answ[0] >= 20 && answ[0] <= 25)
+            {
+                label5.Text = $"第四題結果：最小BMI值={answ[0].ToString("F0")}，正常";
+            }
+            else
+            {
+                label5.Text = $"第四題結果：最小BMI值={answ[0].ToString("F0")}，不正常";
+            }
+            //*******************************
+            //* 11900-1060305 Program Start *
+            //*******************************
+            string[] input5 = File.ReadAllLines("1060305.SM");
+            label6.Text = "第五題結果：\n";
+            for (int i = 0; i < 2; i++)
+            {
+                string[] a = input5[i].Split(',');
+                string[] b = input5[i + 2].Split(',');
+                label6.Text += $"[{int.Parse(a[0]) + int.Parse(b[0])}         {int.Parse(a[1]) + int.Parse(b[1])}]\n";
+            }
+        }
+    }
+}
